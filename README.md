639a82f8ab80b265b586429390ec37953b8b5092
Inna <zib.art@yahoo.com>
2024-01-16 11:39:20 +0100

    weather app created

---
+++ README.md
@@ -0,0 +1,3 @@
+# weather_app_asyc
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/weather_app_asyc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.weather_app_asyc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
